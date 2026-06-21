77b41a978a09df40de9ad45bb0ba9326e8de5111
Shreyan <royshreyan84@gmail.com>
2026-06-21 21:05:24 +0530

    recorrected the video source link

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -193,7 +193,7 @@ export default function WatchPage() {
 
   const [fallbackIndex, setFallbackIndex] = useState(0)
   const fallbackSources = [
-    `https://vidsrc.ru/embed/anime/${animeId}/${currentEp}`,
+    `https://vidsrcme.ru/embed/anime/${animeId}/${currentEp}`,
     `https://anime.autoembed.cc/embed/anime?mal=${animeId}&episode=${currentEp}`,
     `https://vidsrc.xyz/embed/anime?mal=${animeId}&episode=${currentEp}`,
   ]
