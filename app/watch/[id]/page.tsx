3d549e7fd2870aff80796ffc9c9704b2a77de041
Shreyan <royshreyan84@gmail.com>
2026-06-21 21:33:21 +0530

    changed the video source part3

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -193,7 +193,7 @@ export default function WatchPage() {
 
   const [fallbackIndex, setFallbackIndex] = useState(0)
   const fallbackSources = [
-    `https://vsrc.su/embed/anime/${animeId}/${currentEp}`,
+    `https://vidsrc-me.su/embed/anime/${animeId}/${currentEp}`,
     `https://anime.autoembed.cc/embed/anime?mal=${animeId}&episode=${currentEp}`,
     `https://vidsrc.xyz/embed/anime?mal=${animeId}&episode=${currentEp}`,
   ]
